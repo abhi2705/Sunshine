1724d7d27fe4ac63f8193d00c9e2b04219d42d84
Abhinav <abhi.pat93@gmail.com>
2015-11-09 01:53:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/app/SampleTestClass.java
@@ -0,0 +1,25 @@
+package com.example.android.sunshine.app;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Abhinav on 11/8/2015.
+ */
+public class SampleTestClass {
+
+    public static void main(String[] args) {
+
+        /*JSONObject a = new JSONObject();
+        try {
+            a.put("acdav","advdavadva");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println(a);*/
+
+
+    }
+
+
+}
